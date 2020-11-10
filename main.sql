297eee7580a03e404cc916d43b3328b0c459be8d
Jason Zhu <jzhu43@ucmerced.edu>
2020-11-10 15:29:29 -0800

    Finished 20 Queries + Insert/Delete/Update

--- main.sql
+++ main.sql
@@ -88,14 +88,14 @@ GROUP BY m_title, app_role;
 --#7
 SELECT m_title, m_company, d_name
 FROM movies, director, company
-WHERE m_company = c_company 
+WHERE m_company = c_company
     AND d_name = m_director
 GROUP BY d_name
 
 --#8
 SELECT m_title, m_year, a_name, app_role, g_genre, r_imdb, r_rottent
 FROM movies, actor, appears, genre, review
-WHERE m_moveid = app_miveid	
+WHERE m_moveid = app_miveid
     AND movies.m_moveid = review.r_movieid
     AND m_moveid = g_movieid
     AND a_actorid = app_actorid
@@ -186,17 +186,54 @@ WHERE m_moveid = app_miveid
 GROUP BY m_title, a_name;
 
 --#18
-;
+SELECT distinct m_title, m_year, a_name, d_name
+from director, movies, actor
+where d_name = a_name
+    and m_director = d_name
+    and m_year < 1950
+GROUP BY a_name;
+
 --#19
-;
+SELECT m_moveid, m_title, c_company, g_genre
+from genre, company, movies
+where m_company = c_company
+    and g_movieid = m_moveid
+    and g_genre NOT IN (select g_genre
+                        from genre
+                        where g_genre LIKE 'Mystery')
+GROUP BY c_company;
+
 --#20
-;
---#21
-;
+SELECT m_title, g_genre
+from genre, movies, review
+    where r_movieid = m_moveid
+        and g_movieid = m_moveid
+        and r_imdb > 6
+        and r_rottent > 7
+        and g_genre = (select g_genre
+                        from genre
+                        where  g_genre LIKE 'Romance')
+Order by m_title ASC;
+
 
 --# Inserting into a table and the query to show it's located in the table
 INSERT INTO review VALUES ('tt0', '5.1', '5.4');
-
 SELECT r_movieid, r_imdb, r_rottent
 FROM review
 WHERE r_movieid LIKE 'tt0';
+
+INSERT INTO genre(g_movieid, g_genre) VALUES('tt0', 'Western')
+select g_movieid, g_genre
+from genre
+where g_movieid LIKE 'tt0';
+
+--# Deleting from a table with the specified conditions
+DELETE FROM review where r_movieid LIKE 'tt0';
+
+DELETE FROM genre where g_movieid LIKE 'tt0';
+
+--# Updating an existing tuple in the table with the specified conditions
+UPDATE genre SET g_genre = 'Drama' where g_movieid = 'tt0'
+select g_movieid, g_genre
+from genre
+where g_movieid LIKE 'tt0';
