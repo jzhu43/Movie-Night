a676b34fc8f7c9790a9dfe8378597f6b9edd9875
Jason Zhu <jzhu43@ucmerced.edu>
2020-11-11 18:54:40 -0800

    Replace db with changes/changed datatypes entities

--- main.sql
+++ main.sql
@@ -7,35 +7,32 @@
 -- -- CREATE TABLE Appears(
 -- --     actorID varchar(50) primary key not null,
 -- --     movieID varchar(50) not null,
--- --     role varchar(15) not null
+-- --     role varchar(20) not null
 -- -- );
 
 -- -- CREATE TABLE Movie(
 -- --     movieID varchar(50) primary key not null,
--- --     title varchar(40) not null,
+-- --     title varchar(60) not null,
 -- --     year integer not null,
 -- --     length float not null,
 -- --     companyName varchar(40) not null,
--- --     director varchar(50) not null,
--- --     rating varchar(5) not null
-
+-- --     director varchar(50) not null
 -- -- );
 
 -- -- CREATE TABLE Director(
--- --     dirID integer primary key not null,
+-- --     dirID varchar(50) primary key not null,
 -- --     dirName varchar(50) not null,
--- --     dob date not null
 -- -- );
 
 -- -- CREATE TABLE Genre(
--- --     movieID integer primary key not null,
+-- --     movieID varchar(50) primary key not null,
 -- --     genreType varchar(20) not null
 -- -- );
 
 -- -- CREATE TABLE Review(
--- --     movieID integer primary key not null,
--- --     imdb integer not null,
--- --     rottent integer not null
+-- --     movieID varchar(50) primary key not null,
+-- --     imdb float not null,
+-- --     rottent float not null
 -- -- );
 
 -- -- CREATE TABLE Company(
