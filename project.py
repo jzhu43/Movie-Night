fd9bcef0601b9fec4572dd97b0d45053af1391d2
Malia <mbowman3@ucmerced.edu>
2020-11-29 14:55:47 -0800

    Connected the database in project.py

--- project.py
+++ project.py
@@ -1,3 +1,6 @@
+# # Python file for database Final Phase of Project
+# # Created by Malia Bowman and Jason Zhu
+
 import os
 import sqlite3
 from sqlite3 import Error
@@ -6,21 +9,49 @@ import random
 
 ################ Connect SQLite Database ################
 
-db_connection = None # Define the connection parameter
-#db_name = "/Users/bowma/CSE 111/Project/CSE111-Project-F20/moviesdata.db" # Specify the full path of Database file
-db_name = "/Users/bowma/CSE 111/Project/CSE111-Project-F20/project.db"
-#db_name = "moviesdata.db"
+# db_connection = None # Define the connection parameter
+# #db_name = "/Users/bowma/CSE 111/Project/CSE111-Project-F20/moviesdata.db" # Specify the full path of Database file
+# db_name = "/Users/bowma/CSE 111/Project/CSE111-Project-F20/project.db"
+# #db_name = "moviesdata.db"
 
-# def openConnection(_dbFile): 
-#     print("++++++++++++++++++++++++++++++++++")
-#     print("Open database: ", _dbFile)
-#     conn = None
-#     try:
-#         conn = sqlite3.connect(_dbFile)
-#         print("success")
-#     except Error as e:
-#         print(e)
 
-#     print("++++++++++++++++++++++++++++++++++")
+def openConnection(_dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Open database: ", _dbFile)
 
-#     return conn
+    conn = None
+    try:
+        conn = sqlite3.connect(_dbFile)
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+    return conn
+
+
+def closeConnection(_conn, _dbFile):
+    print("++++++++++++++++++++++++++++++++++")
+    print("Close database: ", _dbFile)
+
+    try:
+        _conn.close()
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def main():
+   database = r"moviesdata.db"
+
+   # create a database connection
+   conn = openConnection(database)
+   
+
+   closeConnection(conn, database)
+
+
+if __name__ == '__main__':
+    main()
